1d2b368ac9daef60cc02c7e9f3462b11781897ff
Justin Adams <146144016+Just-A-404-waiting-to-happen@users.noreply.github.com>
2023-11-20 19:24:27 -0700

    Update userScript.js
    Added function to toggle the filter panel on and off. 
    Added function to get a list of filters Options.

--- public_html/users/userScript.js
+++ public_html/users/userScript.js
@@ -8,20 +8,39 @@
 
 const local = "http://localhost:80/"
 const live = "http:/146.190.45.141:80/"
-//"http:/146.190.45.141:80/" swap this for local when ready to used Dig-Ocean
+//"http:/146.190.45.141:80/" swap this for local when ready to use Dig-Ocean
 const urlRoot = live
 
 
-/*This funtion will be called when open pantry is click on the 
+/*This function will be called when the open pantry is clicked on the 
  user's home page from here it opens a page that lets the user update
- what items are in thier pantry. 
+ what items are in their pantry. 
 */
 function openPantry(){
 	
 }
 
-/*Update panrty is called after update is clicked on users pantry 
-form it sends all the data to the server to update user's ingredients */
+
+/*Update pantry is called after update is clicked on the user's pantry 
+form it sends all the data to the server to update the user's ingredients */
 function updatePantry(){
 	
+}
+/*open Filter just toggles the */
+function openFilter(){ document.getElementById("filterPanel").style.display= "block"; }
+
+/*filterSubmint collects all the elements of class filterOpt(ions)
+and adds the names to a list ready to be sent to the server. */
+function filterSubmint(){
+	let parts = document.getElementsByClassName("filterOpt");
+	let names = []; 
+	for (let i = 0 ; i < parts.length ; i++){
+		if(parts[i].checked){
+			parts[i].checked = false;
+			names.push(parts[i].name);
+		}
+	}
+	console.log(names);
+	/* send names to server */
+	document.getElementById("filterPanel").style.display= "none";
 }
