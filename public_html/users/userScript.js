20a1334dbebaf9b5edcc38545a385375887b1321
Justin Adams <146144016+Just-A-404-waiting-to-happen@users.noreply.github.com>
2023-11-26 20:38:19 -0700

    Update userScript.js
    Fixed getAllRecipes to correctly return data
    updated code to work with open filters, and submit filters.

--- public_html/users/userScript.js
+++ public_html/users/userScript.js
@@ -6,116 +6,62 @@
 */
 
 
-const local = "http://localhost:80/"
-const live = "http:/146.190.45.141:80/"
-//"http:/146.190.45.141:80/" swap this for local when ready to used Dig-Ocean
-const urlRoot = live
-
 
 /*Populate Recipes to contentPanel is used to update the bookshelf page
 with all/search/filter results. */
-function populateRecipes(recipes) {
-		console.log("populate: "+ recipes);
-        document.getElementById("contentPanel").innerHTML = formatRecipes(r);
+function populateRecipes(rep) {
+		//debug console.log("populate: "+ rep);
+        	document.getElementById("contentPanel").innerHTML = formatRecipes(rep);
 }
 
 /**
  * Function: getAllRecipes
  * Purpose:  This function will make a GET request to the server to get
- *           all recipes.
+ *           All recipes.
  * 
  * Parameters:   N/A
- * Returns:      N/A
+ * Returns:      array of Recipe Objs
  */
 async function getAllRecipes() {
-    let url = "get/recipes/browse";
-    await fetch(url, {
+    let url =  "/get/recipes/browse";
+    let p = await fetch(url, {
         method: "GET",
         headers: {"Content-Type": "application/json"}
     })
     .then((response) => {
-			return  response.json();
+	    return  response.json();
     }).then((data) => {
-        console.log("All Recipes: " + JSON.stringify(data));
-		return(data);
+        //debug console.log("All Recipes: " + JSON.stringify(data));
+	    return data;
     }).catch((err) => {
-        window.alert("Error getting all recipes.");
+	window.alert("Error getting all recipes. "+err);
     });
+    return p;
 }
-
-let r = [
-  {
-  "name": "Grilled Cheese",
-  "short_description": "A classic comfort food featuring melted cheese between slices of golden-brown bread.",
-  "long_description": "Grilled Cheese is a simple yet satisfying comfort food loved by many. This recipe features your choice of cheese melted between slices of buttery, golden-brown bread.",
-  "ingredients": [
-    "8 slices Bread",
-    "8 oz Cheese (cheddar, Swiss, or your favorite melting cheese)",
-    "4 tbsp Butter, softened"
-  ],
-  "instructions": [
-    "Place a skillet or griddle over medium heat.",
-    "Spread butter on one side of each slice of bread.",
-    "Place a slice of bread, butter side down, on the skillet.",
-    "Add a generous amount of cheese on top of the bread.",
-    "Place another slice of bread on top, butter side up.",
-    "Cook until the bottom bread is golden brown and the cheese is melting.",
-    "Flip and cook the other side until golden brown and the cheese is fully melted.",
-    "Repeat for the remaining slices of bread and cheese.",
-    "Slice the grilled cheese sandwiches diagonally and serve hot."
-  ],
-  "cookTime": 15,
-  "difficulty": 1,
-  "images": [ 
-    "GrilledCheese1.png"
-]
-
-  },{
-  "name": "Stir-Fried Tofu with Vegetables",
-  "short_description": "A quick and flavorful stir-fry featuring tofu, colorful vegetables, and a savory soy sauce.",
-  "long_description": "Stir-Fried Tofu with Vegetables is a delicious and nutritious dish that combines tofu, bell peppers, carrots, and broccoli in a savory soy sauce. Quick and easy, it's perfect for a weeknight dinner.",
-  "ingredients": [
-    "1 block Tofu (extra-firm)",
-    "3 tbsp Soy sauce",
-    "2 Bell peppers, sliced",
-    "2 Carrots, julienned",
-    "1 cup Broccoli, florets",
-    "2 cloves Garlic, minced",
-    "1 tsp Ginger, grated"
-  ],
-  "instructions": [
-    "Press tofu to remove excess water.",
-    "Stir-fry tofu, garlic, and ginger in soy sauce.",
-    "Add sliced vegetables and cook until tender."
-  ],
-  "cookTime": 20,
-  "difficulty": 1,
-  "images": ["StirFriedTofuVeg.png"]
-}
-]
 
 /*Format recipes used to turn the data into string/HTML element*/
 function formatRecipes(recipes){
 	let htmlString = "";
 	for(let recipe of recipes){
-		console.log(recipe);
+		//debug console.log(recipe);
+		htmlString +='<a href="#" class="'+
+			'recipeLink" onclick = "openRecipe('+recipe+')" >' /*rework how openRecipe is called */
 		htmlString += '<div class="Panel bookshelfPanel">';
 		htmlString +='<h2 class="recipeTitles">'+recipe.name+'</h2>';
-		
+		//need to be updated for sizes in CSS at some point. 
 		htmlString +=
-		'<img src="./Data/img/'
+		'<div><img class="smallImage" src="../img/'
 			+recipe.images[0]+'" alt="'
-			+recipe.images[0]+'"><br>';
-
+			+recipe.images[0]+'">';
 		htmlString +=
-		'<br><p class="shortDesc">'+recipe.short_description+'</p>';
-
+			'<p class="shortDesc">'+recipe.short_description+'</p></div>';
+		
 		let ct = minutes2Sting(recipe.cookTime);
 		htmlString +=
 		'<p class="cookTime">Cook Time: '+ ct +'</p>';
 
 		htmlString +=
-			'<p id="difficulty">Difficulty: '+recipe.difficulty+'</p></div>';
+			'<p id="difficulty">Difficulty: '+recipe.difficulty+'</p></div></a>';
 	}
 	return htmlString;
 }
@@ -128,10 +74,32 @@ function minutes2Sting(mins){
 	return outString;
 }
 
+/*open Filter just toggles the */
+function openFilter(){
+	document.getElementById("filterPanel").style.display= "block";
+}
 
-/*This funtion will be called when open pantry is click on the 
+/*filterSubmint collects all the elements of class filterOpt(ions)
+and adds the names to a list ready to be sent to the server. */
+function filterSubmint(){
+	let parts = document.getElementsByClassName("filterOpt");
+	let names = []; 
+	for (let i = 0 ; i < parts.length ; i++){
+		if(parts[i].checked){
+			parts[i].checked = false;
+			names.push(parts[i].name);
+		}
+	}
+	if(names.length > 0){
+		console.log(names);
+		/* send names to server */
+	}
+	document.getElementById("filterPanel").style.display= "none";
+}
+
+/*This function will be called when open pantry is clicked on the 
  user's home page from here it opens a page that lets the user update
- what items are in thier pantry. 
+ what items are in their pantry. 
 */
 function openPantry(){
 	
